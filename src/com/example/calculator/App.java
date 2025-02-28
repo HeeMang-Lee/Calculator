b037a5a0285074581c9c0c608d1f5cbe3ca83ea8
hemsej018 <hemsej018@gmail.com>
2025-02-28 11:43:13 +0900

    refactor(app): 연산 수행을 Calculator 클래스에서 처리하도록 변경

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         while (true) {
             System.out.println("\n 계산을 수행합니다.");
@@ -19,7 +20,10 @@ public class App {
             int operatorSymbol = getOperatorSymbol(scanner);
 
             //연산 수행
-            calculateOperate(firstNumber, secondNumber, operatorSymbol);
+            int result = calculator.calculate(firstNumber, secondNumber, operatorSymbol);
+            if (result != Integer.MIN_VALUE) {
+                System.out.println("연산 결과 : " + result);
+            }
 
             //실행 여부 확인
             System.out.print("\n 계속 이용하려면 아무 키나 입력하세요, 종료하려면 'exit' 입력: ");
@@ -73,36 +77,36 @@ public class App {
         }
     }
 
-        // 연산 수행
-    public static int calculateOperate(int firstNumber, int secondNumber, int operatorSymbol) {
-        int result = 0;
-        switch (operatorSymbol) {
-            case 1:
-                result = firstNumber + secondNumber;
-                System.out.println("덧셈 결과: " + result);
-                break;
-            case 2:
-                result = firstNumber - secondNumber;
-                System.out.println("뺄셈 결과: " + result);
-                break;
-            case 3:
-                result = firstNumber * secondNumber;
-                System.out.println("곱셈 결과: " + result);
-                break;
-            case 4:
-                if (secondNumber == 0) {
-                    System.out.println("0으로 나눌 수 없습니다!");
-                    return Integer.MIN_VALUE;
-                } else {
-                    result = firstNumber / secondNumber;
-                    System.out.println("나눗셈 결과: " + result);
-                }
-                break;
-            default:
-                System.out.println("올바른 연산자를 입력하세요.");
-                return Integer.MIN_VALUE;
-        }
-        return result;
-    }
+//        // 연산 수행
+//    public static int calculateOperate(int firstNumber, int secondNumber, int operatorSymbol) {
+//        int result = 0;
+//        switch (operatorSymbol) {
+//            case 1:
+//                result = firstNumber + secondNumber;
+//                System.out.println("덧셈 결과: " + result);
+//                break;
+//            case 2:
+//                result = firstNumber - secondNumber;
+//                System.out.println("뺄셈 결과: " + result);
+//                break;
+//            case 3:
+//                result = firstNumber * secondNumber;
+//                System.out.println("곱셈 결과: " + result);
+//                break;
+//            case 4:
+//                if (secondNumber == 0) {
+//                    System.out.println("0으로 나눌 수 없습니다!");
+//                    return Integer.MIN_VALUE;
+//                } else {
+//                    result = firstNumber / secondNumber;
+//                    System.out.println("나눗셈 결과: " + result);
+//                }
+//                break;
+//            default:
+//                System.out.println("올바른 연산자를 입력하세요.");
+//                return Integer.MIN_VALUE;
+//        }
+//        return result;
+//    }
 }
 
