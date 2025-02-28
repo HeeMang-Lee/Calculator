38173939f7636cc6acdb93c8a4cf24a7b12e82d5
hemsej018 <hemsej018@gmail.com>
2025-02-28 11:02:54 +0900

    feat(calculator): 사칙연산 수행 및 연산 결과 저장 기능 추가

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,4 +1,42 @@
 package com.example.calculator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Calculator {
+    // 속성
+    private List<Integer> results; // 연산 결과를 저장하는 컬렉션 타입 리스트 생성
+
+    // 생성자
+    public Calculator() {
+        this.results = new ArrayList<>();
+    }
+
+    // 기능
+    public int calculate(int firstNumber, int secondNumber, int operatorSymbol) {
+        int result = 0;
+        switch (operatorSymbol) {
+            case 1:
+                result = firstNumber + secondNumber;
+                break;
+            case 2:
+                result = firstNumber - secondNumber;
+                break;
+            case 3:
+                result = firstNumber * secondNumber;
+                break;
+            case 4:
+                if (secondNumber == 0){
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return Integer.MIN_VALUE;
+                }
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력하세요");
+                return Integer.MIN_VALUE;
+        }
+        this.results.add(result); //연산 결과 저장
+        return result;
+    }
 }
