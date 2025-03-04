186e891e15749b2ae773561f719d04761a5d98a3
hemsej018 <hemsej018@naver.com>
2025-03-04 17:20:48 +0900

    feat: ArithmeticCalculator 클래스를 추가하여 사칙연산 기능구현

--- src/com/example/calculator/ArithmeticCalculator.java
+++ src/com/example/calculator/ArithmeticCalculator.java
@@ -1,4 +1,39 @@
 package com.example.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArithmeticCalculator {
+    // 속성
+    private final List<Integer> results;
+
+    // 생성자
+    public ArithmeticCalculator() {
+        this.results = new ArrayList<>();
+    }
+
+    // 기능
+    public int calculate(int firstNumber,int secondNumber,OperatorType operator) {
+        int result = operator.apply(firstNumber,secondNumber);
+        return result;
+    }
+
+    // 게터 기능 활용 : 연산 기록 조회
+    public List<Integer> getResults(){
+        return results;
+    }
+
+    // 세터 기능 활용 : 연산 결과 저장
+    public void addResults(int result){
+        this.results.add(result);
+    }
+
+    // 저장된 연산 기록 삭제 (FIFO)
+    public void removeResults(){
+        if (!results.isEmpty()){ // 빈 리스트 예외 방지
+            results.remove(0); // 가장 먼저 등록된 연산 결과 삭제
+        }
+    }
+
+
 }
