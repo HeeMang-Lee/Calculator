129ab2a796350e609a8eaeb0a506911fbe0c7661
hemsej018 <hemsej018@naver.com>
2025-03-04 17:42:39 +0900

    fix(operator): 누락된 MULTIPLY 연산자 추가

--- src/com/example/calculator/OperatorType.java
+++ src/com/example/calculator/OperatorType.java
@@ -13,6 +13,12 @@ public enum OperatorType {
             return a - b;
         }
     },
+    MULTIPLY("*"){
+        @Override
+        public int apply(int a, int b){
+            return a * b;
+        }
+    },
     DIVIDE("/"){
         @Override
         public int apply(int a, int b){
